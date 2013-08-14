d66c5d8d82fd02b691f68afef9340d1fbf926caf
Zheng Gao <gaozhengwh@163.com>
2013-08-14 23:46:54 +0100

    Format

--- BBS/src/com/wang/objects/Article.java
+++ BBS/src/com/wang/objects/Article.java
@@ -3,67 +3,100 @@ package com.wang.objects;
 import java.sql.Timestamp;
 import java.util.Date;
 
-public class Article {
-	private int id;
-	private int pid;
-	private int rootid;
-	private String title;
-	private String cont;
-	private Timestamp pdate; //sql date 是util子集
-	private Boolean isleaf;
-	public Article() {
-		super();
-	}
-	public Article( int pid, int rootid, String title, String cont,
-			Timestamp pdate, Boolean isleaf) {
-		super();
-		this.pid = pid;
-		this.rootid = rootid;
-		this.title = title;
-		this.cont = cont;
-		this.pdate = pdate;
-		this.isleaf = isleaf;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getPid() {
-		return pid;
-	}
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-	public int getRootid() {
-		return rootid;
-	}
-	public void setRootid(int rootid) {
-		this.rootid = rootid;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getCont() {
-		return cont;
-	}
-	public void setCont(String cont) {
-		this.cont = cont;
-	}
-	public Timestamp getPdate() {
-		return pdate;
-	}
-	public void setPdate(Timestamp pdate) {
-		this.pdate = pdate;
-	}
-	public Boolean getIsleaf() {
-		return isleaf;
-	}
-	public void setIsleaf(Boolean isleaf) {
-		this.isleaf = isleaf;
-	}
+public class Article
+{
+    private int id;
+    private int pid;
+    private int rootid;
+    private String title;
+    private String cont;
+    private Timestamp pdate; // sql date 是util子集
+    private Boolean isleaf;
+
+    public Article()
+    {
+	super();
+    }
+
+    public Article(int pid, int rootid, String title, String cont,
+	    Timestamp pdate, Boolean isleaf)
+    {
+	super();
+	this.pid = pid;
+	this.rootid = rootid;
+	this.title = title;
+	this.cont = cont;
+	this.pdate = pdate;
+	this.isleaf = isleaf;
+    }
+
+    public int getId()
+    {
+	return id;
+    }
+
+    public void setId(int id)
+    {
+	this.id = id;
+    }
+
+    public int getPid()
+    {
+	return pid;
+    }
+
+    public void setPid(int pid)
+    {
+	this.pid = pid;
+    }
+
+    public int getRootid()
+    {
+	return rootid;
+    }
+
+    public void setRootid(int rootid)
+    {
+	this.rootid = rootid;
+    }
+
+    public String getTitle()
+    {
+	return title;
+    }
+
+    public void setTitle(String title)
+    {
+	this.title = title;
+    }
+
+    public String getCont()
+    {
+	return cont;
+    }
+
+    public void setCont(String cont)
+    {
+	this.cont = cont;
+    }
+
+    public Timestamp getPdate()
+    {
+	return pdate;
+    }
+
+    public void setPdate(Timestamp pdate)
+    {
+	this.pdate = pdate;
+    }
+
+    public Boolean getIsleaf()
+    {
+	return isleaf;
+    }
+
+    public void setIsleaf(Boolean isleaf)
+    {
+	this.isleaf = isleaf;
+    }
 }
--- BBS/test/com/wang/test/GetArticleTest.java
+++ BBS/test/com/wang/test/GetArticleTest.java
@@ -8,20 +8,22 @@ import org.junit.Test;
 import com.wang.objects.Article;
 import com.wang.service.hibernate.ArticlesDBService;
 
-public class GetArticleTest {
+public class GetArticleTest
+{
 
-	@Test
-	public void test() {
-		List<Article> articles = new ArrayList<Article>(0);
-		articles =  ArticlesDBService.treePopulator(articles, 0);
-		
-		System.out.println(articles.size()+ ""+articles.get(0).getId());
-		
-		Article article =  ArticlesDBService.getArticle(1);
-		System.out.println(article.getId()+ article.getCont());
-		
-		ArticlesDBService.saveArticle(1,"1231","123");
+    @Test
+    public void test()
+    {
+	List<Article> articles = new ArrayList<Article>(0);
+	articles = ArticlesDBService.treePopulator(articles, 0);
 
-	}
+	System.out.println(articles.size() + "" + articles.get(0).getId());
+
+	Article article = ArticlesDBService.getArticle(1);
+	System.out.println(article.getId() + article.getCont());
+
+	ArticlesDBService.saveArticle(1, "1231", "123");
+
+    }
 
 }
