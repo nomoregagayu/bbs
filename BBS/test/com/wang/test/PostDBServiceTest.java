11674f660f17dffdceb967ff83b362942c6284f4
303117251 <wangmengyang101@gmailcom>
2013-09-13 16:03:32 +0100

    maven +log4j

--- BBS/test/com/wang/test/PostDBServiceTest.java
+++ BBS/test/com/wang/test/PostDBServiceTest.java
@@ -1,14 +1,21 @@
 package com.wang.test;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.wang.service.impl.ReplyPageManagerImpl;
 
 
 public class PostDBServiceTest {
+	
+	
 	@Test
 	public void testReply() {
 		ReplyPageManagerImpl pageManager =ReplyPageManagerImpl.getInstance();
-		pageManager.add(1, "asdasd", "asdasd");
+		pageManager.add(0, "asdasd", "asdasd");
 	}
+
 }
